fafe830db29e773e4e939f0afdfe91d5d1e4202a
Thomas Buhrmann <thomas@graphext.com>
2025-10-02 13:03:12 +0200

    Print funnel actor validation errors.

--- src/cuery/actors/funnel.py
+++ src/cuery/actors/funnel.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 
 from apify import Actor
+from apify_client._errors import ApifyApiError
 from pandas import DataFrame
 
 from ..seo.funnels import Funnel
@@ -23,7 +24,14 @@ async def main():
 
         df: DataFrame = result
         records = json.loads(df.to_json(orient="records", date_format="iso", index=False))
-        await Actor.push_data(records)
+
+        try:
+            await Actor.push_data(records)
+        except ApifyApiError as error:
+            if "invalidItems" in error.data:
+                validation_errors = error.data["invalidItems"]
+                print(validation_errors)
+                raise
 
 
 if __name__ == "__main__":
